28fee4b0e3245121c4cd80772ba09db506a03c8e
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-07-05 04:59:54 -0700

    Add 'user' patch
    This patch uses the $USER environment variable to determine which user
    and group to use for the password instead of having to rely on fixed
    configuration values.

--- config.h
+++ config.h
@@ -1,7 +1,3 @@
-/* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
-
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
